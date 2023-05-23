632d2404248a8df3ded8b77faa3918f1b4347ee6
Kasif17 <developer786kasif@gmail.com>
2023-05-23 14:20:42 +0530

    first commite

---
+++ config/mongoose.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+// connecting mongoose to its default server and ecommerceDB
+//mongoose.connect('mongodb+srv://soumyasri2245:Soumya22%4034@cluster0.u2ywt3o.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect('mongodb://localhost:27017/E_Commerce_api'  ,  {
+useNewUrlParser: true
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+
+db.once('open', function(){
+    console.log('Connected to Database :: MongoDB');
+});
+
+module.exports = db;
